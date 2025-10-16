10804a3e4ab012c2cb117531e2d4568158e26b12
MR-RITZY <mrboffin01@gmail.com>
2025-10-16 13:41:10 +0100

    config update

--- scr/config.py
+++ scr/config.py
@@ -1,16 +1,19 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
-    email:str
-    name:str
-    redis_db:str
-    redis_host:str
-    redis_password:str
-    redis_port:str
-    redis_username:str
-    stack:str
-    
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", env_ignore_empty=True)
+    email: str
+    name: str
+    redis_db: str
+    redis_host: str
+    redis_password: str
+    redis_port: str
+    redis_username: str
+    stack: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
 
 
-settings = Settings(_env_file=None)
+settings = Settings()
